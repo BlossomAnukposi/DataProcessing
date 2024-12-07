932354b5febca9fd2cab171f828de456e05be0be
blossomAnukposi <blossom.anukposi1@student.nhlstenden.com>
2024-12-07 02:36:38 +0100

    Updated start script for deployment

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
+
+//"test": "echo \"Error: no test specified\" && exit 1"
+//removes from package.json
