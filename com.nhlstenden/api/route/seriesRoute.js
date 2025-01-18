587ae70f88fc45c68b1835765aa87d988589c541
Mihail177 <mihail.josan@student.nhlstenden.com>
2025-01-18 17:23:05 +0100

    Fixed create series and added the test script for postman

--- com.nhlstenden/api/route/seriesRoute.js
+++ com.nhlstenden/api/route/seriesRoute.js
@@ -2,14 +2,13 @@ const express = require("express");
 const router = express.Router();
 const SeriesController = require("../controller/seriesController");
 
-router.get('/', SeriesController.getAllEntries);
-router.get('/:id', SeriesController.getEntryById);
-router.delete('/:id', SeriesController.deleteEntryById);
-// router.post('/', SeriesController.createSeries);
-// router.patch('/:id', SeriesController.updateSeries);
+router.get("/", SeriesController.getAllEntries);
+router.get("/:id", SeriesController.getEntryById);
+router.delete("/:id", SeriesController.deleteEntryById);
+router.post("/", SeriesController.createSeries);
+router.put("/:id", SeriesController.updateSeries);
 
 // series seasons
 // total episodes, total duration
-
 
 module.exports = router;
